101e1919156488a2b6b45d330df6605fa751cd9f
dannimajdalani <dani.m@vanillaforums.com>
2021-01-13 23:31:42 -0500

    cleanup

--- src/Utils/StationDateTime.php
+++ src/Utils/StationDateTime.php
@@ -21,13 +21,13 @@ class StationDateTime {
      * Time H:i:s
      *
      * @param string $duration
-     * @param bool $formated Return the dateTime object, or a formatted date or time.
+     * @param bool $formatted If True, Return Data Or Time formatted string, otherwise Return DateTime formated object
      * @param string $format
      * @param string $timeZone
      * @return string
      * @throws \Exception
      */
-    public static function dateNow($duration = '', bool $formatted = false, $format = 'Y-m-d H:i:s', $timeZone = 'America/Toronto') {
+    public static function dateNow(string $duration = '', bool $formatted = false, $format = 'Y-m-d H:i:s', string $timeZone = 'America/Toronto') {
         if ($duration !== '') {
             $period = new \DateInterval($duration);
         }
