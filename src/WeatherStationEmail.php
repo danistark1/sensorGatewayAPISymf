6cd2226d262fb4ec74ba6da354406b4e899b44ec
dannimajdalani <dani.m@vanillaforums.com>
2021-01-19 20:36:10 -0500

    add back the email class

---
+++ src/WeatherStationEmail.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * @author Dani Stark.
+ */
+namespace App;
+
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+
+class WeatherStationEmail {
+
+    /** @var string */
+    public  $from;
+
+    /** @var string */
+    public  $to;
+
+    /** @var string */
+    public  $cc;
+
+    /** @var string */
+    public  $bcc;
+
+    public  $template;
+
+    public  $emailData;
+    private $mailer;
+    private $templating;
+
+    /**
+     * WeatherStationEmail constructor.
+     *
+     * @param $from
+     * @param array $to
+     * @param array $cc
+     * @param array $bcc
+     */
+    public function __Construct(
+        \Twig\Environment $templating,
+        MailerInterface $mailer,
+        $from,
+        array $to,
+        $template,
+        array $emailData
+    ) {
+        $this->templating = $templating;
+        $this->mailer = $mailer;
+        $this->from = $from;
+        $this->to = $to;
+        $this->prepareEmail();
+    }
+
+    /**
+     *
+     */
+    private function prepareEmail() {
+
+        $email = (new Email())
+            ->from($this->from)
+            ->to($this->to)
+            ->html(
+                $this->templating->render(
+                    $this->template,
+                    ['stationData' => $this->emailData]
+                ),
+                'text/html'
+            );
+        $this->mailer->send($email);
+    }
+}
